5b088d9444438b7ca11b156b6eb5d60cc0635342
lokasan <lokasan@inbox.ru>
2025-05-09 19:38:02 +0300

    added core function mock

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ def main(service_name: str) -> str:
     return f'Name service: {service_name}'
 
 
+def core(message: str, _type: int):
+    return f'Msg: {message} Type: {_type}'
+
+
 if __name__ == '__main__':
     msg = main('Linera Cards')
     print(msg)
